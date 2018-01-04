e91dc2d05042b9e08e2b5f241500d52f052c331b
milsevol <15013272917@163.com>
2018-01-04 22:51:44 +0800

    @Inject测试

--- springidol-autodiscovery/src/main/java/com/vonzhou/springinaction/springidol/KinfeJupper.java
+++ springidol-autodiscovery/src/main/java/com/vonzhou/springinaction/springidol/KinfeJupper.java
@@ -1,19 +1,32 @@
 package com.vonzhou.springinaction.springidol;
 
+import org.springframework.stereotype.Component;
+
 import javax.inject.Inject;
-import javax.xml.ws.Provider;
+import javax.inject.Provider;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
  * Created by xueyong.cui on 2018/1/4.
  */
+@Component("kinfeJupper")
 public class KinfeJupper {
     private Set<Knife> knives;
 
     @Inject
     public KinfeJupper(Provider<Knife> knifeProvider){
+        knives = new HashSet<Knife>();
         for(int i=0;i<5;i++){
             knives.add(knifeProvider.get());
+        }
+    }
+    public void KinfeJupper(){
+        Iterator<Knife> its = knives.iterator();
+        while (its.hasNext()) {
+            Knife it = its.next();
+            it.play();
         }
     }
 
---
+++ springidol-autodiscovery/src/test/java/com/vonzhou/springinaction/springidol/KinfeJupperTest.java
@@ -0,0 +1,7 @@
+package com.vonzhou.springinaction.springidol;
+
+/**
+ * Created by xueyong on 2018/1/4.
+ */
+public class KinfeJupperTest {
+}
