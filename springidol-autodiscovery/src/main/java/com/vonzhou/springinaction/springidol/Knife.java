0401cfa2d474e93e3f0e8300bed5ae02529066ad
milsevol <15013272917@163.com>
2018-01-04 22:57:58 +0800

    提交knife

--- springidol-autodiscovery/src/main/java/com/vonzhou/springinaction/springidol/Knife.java
+++ springidol-autodiscovery/src/main/java/com/vonzhou/springinaction/springidol/Knife.java
@@ -1,7 +1,13 @@
 package com.vonzhou.springinaction.springidol;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by xueyong.cui on 2018/1/4.
  */
+@Component("Knife")
 public class Knife {
+    public void play() {
+        System.out.println("Knife strum strum");
+    }
 }
